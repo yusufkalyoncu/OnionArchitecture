9e4781bec709bbc2c941d35bfc0454c7e9c01248
yusufkalyoncu <yusufkalyoncuu@gmail.com>
2024-06-01 20:33:41 +0300

    add: serilog and sink seq

---
+++ Presentation/OnionArchitecture.API/Middlewares/RequestLogContextMiddleware.cs
@@ -0,0 +1,22 @@
+using Serilog.Context;
+
+namespace OnionArchitecture.API.Middlewares;
+
+public class RequestLogContextMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public RequestLogContextMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public Task InvokeAsync(HttpContext context)
+    {
+        using (LogContext.PushProperty("CorrelationId", context.TraceIdentifier))
+        {
+            return _next(context);
+        }
+    }
+    
+}
